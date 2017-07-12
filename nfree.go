34a717d5fdb93ce55002189f793415d2bdf06c9d
Brian Sorahan <bsorahan@gmail.com>
2017-07-12 16:29:27 -0500

    simplify nfree

--- nfree.go
+++ nfree.go
@@ -4,38 +4,35 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/scgolang/sc"
 )
 
 // Nfree frees a node.
 type Nfree struct {
-	Controls map[string]float32
-	NodeID   int
-
 	flagErrorHandling flag.ErrorHandling
 	scc               *sc.Client
 }
 
 // Run runs the command.
 func (nfree Nfree) Run(args []string) error {
-	fs := flag.NewFlagSet("nfree", flagErrorHandling)
-	fs.IntVar(&nfree.NodeID, "id", 0, "node ID")
-	if err := fs.Parse(args); err != nil {
+	if len(args) == 0 {
 		return ErrUsage
 	}
-	if nfree.NodeID <= 0 {
+	nodeID, err := strconv.ParseInt(args[0], 10, 32)
+	if err != nil {
 		return ErrUsage
 	}
-	return nfree.scc.NodeFree(int32(nfree.NodeID))
+	if nodeID <= 0 {
+		return ErrUsage
+	}
+	return nfree.scc.NodeFree(int32(nodeID))
 }
 
 // Usage prints a usage message.
 func (nfree Nfree) Usage() {
 	fmt.Fprint(os.Stderr, `
-scc [GLOBAL OPTIONS] nfree [OPTIONS]
-
-OPTIONS
-  -id    (REQUIRED) Node ID.
+scc [GLOBAL OPTIONS] nfree NODE_ID
 `)
 }
